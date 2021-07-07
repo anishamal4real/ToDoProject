10c712c9e41151d1a9047e49c80de70d78a7c758
Anis Hamal Blanco <anishamal4real@gmail.com>
2021-07-07 21:40:44 +0545

    Added Authentication

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
-from .views import TodoCompletedList,TodoList,TodoRetrieveUpdateDestroy, TodoComplete
+from .views import TodoCompletedList,TodoList,TodoRetrieveUpdateDestroy, TodoComplete, signup
 
 urlpatterns=[
     path('todos/completed/',views.TodoCompletedList.as_view()),
     path('todos/',views.TodoList.as_view()),
     path('todos/<int:pk>/complete/',views.TodoComplete.as_view()),
     path('todos/<int:pk>/',views.TodoRetrieveUpdateDestroy.as_view()),
+    path('signup/',views.signup),
 
 ]
--- api/views.py
+++ api/views.py
@@ -1,11 +1,45 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
 from rest_framework import generics,permissions
 from .serializers import TodoSerializer, TodoCompleteSerializer
 from todo.models import Todo
 from django.utils import timezone
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from rest_framework.authtoken.models import Token
+from django.contrib.auth import authenticate
 
+@csrf_exempt
+def signup(request):
+    if request.method == 'POST':
+        try:
+            data = JSONParser().parse(request)
+            user = User.objects.create_user(data['username'], password=data['password'])
+            user.save()
+            token = Token.objects.create(user=user)
+            return JsonResponse({'token':str(token)}, status=201)
+        except IntegrityError:
+            return JsonResponse({'error':'That username has already been taken. Please choose a new username'}, status=400)
 
-# Create your views here.
+@csrf_exempt
+def login(request):
+    if request.method == 'POST':
+        data = JSONParser().parse(request)
+        user = authenticate(request, username=data['username'], password=data['password'])
+        if user is None:
+            return JsonResponse({'error':'Could not login. Please check username and password'}, status=400)
+        else:
+            try:
+                token = Token.objects.get(user=user)
+            except:
+                token = Token.objects.create(user=user)
+            return JsonResponse({'token':str(token)}, status=200)
 
 class TodoCompletedList(generics.ListAPIView):
     serializer_class=TodoSerializer
